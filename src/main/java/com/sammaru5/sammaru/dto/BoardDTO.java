612a8d9f639683fb032b07e6f3333413c42bd1ff
genius-jo <joks7285@daum.net>
2022-03-13 17:37:23 +0900

    fix : Add getter

--- src/main/java/com/sammaru5/sammaru/dto/BoardDTO.java
+++ src/main/java/com/sammaru5/sammaru/dto/BoardDTO.java
@@ -1,7 +1,9 @@
 package com.sammaru5.sammaru.dto;
 
 import com.sammaru5.sammaru.domain.BoardEntity;
+import lombok.Getter;
 
+@Getter
 public class BoardDTO {
 
     private Long boardId;
@@ -9,6 +11,7 @@ public class BoardDTO {
     private String description;
 
     public BoardDTO(BoardEntity board) {
+        this.boardId = board.getId();
         this.boardname = board.getBoardname();;
         this.description = board.getDescription();
     }

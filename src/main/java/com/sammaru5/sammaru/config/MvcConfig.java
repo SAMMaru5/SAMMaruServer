539cecbbd0760160b299d1a9f856153e817fb019
genius-jo <joks7285@daum.net>
2022-03-13 18:57:59 +0900

    feature : Add MvcConfig to resolve CORS

---
+++ src/main/java/com/sammaru5/sammaru/config/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.sammaru5.sammaru.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    private final long MAX_AGE_SECS = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
+    }
+}

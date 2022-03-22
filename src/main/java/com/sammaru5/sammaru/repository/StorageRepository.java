a632ed8ba510231ec47705f708d0b80323d6b299
genius-jo <joks7285@daum.net>
2022-03-22 18:47:28 +0900

    feature : Delete all files belonging to Article

--- src/main/java/com/sammaru5/sammaru/repository/StorageRepository.java
+++ src/main/java/com/sammaru5/sammaru/repository/StorageRepository.java
@@ -5,8 +5,10 @@ import com.sammaru5.sammaru.domain.StorageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StorageRepository extends JpaRepository<StorageEntity, Long> {
 
     List<StorageEntity> findByArticle(ArticleEntity articleEntity);
+    Optional<StorageEntity> findByFilePath(String FilePath);
 }
--- src/main/java/com/sammaru5/sammaru/service/storage/FileRemoveService.java
+++ src/main/java/com/sammaru5/sammaru/service/storage/FileRemoveService.java
@@ -1,24 +1,48 @@
 package com.sammaru5.sammaru.service.storage;
 
+import com.sammaru5.sammaru.domain.ArticleEntity;
+import com.sammaru5.sammaru.domain.BoardEntity;
+import com.sammaru5.sammaru.domain.StorageEntity;
 import com.sammaru5.sammaru.exception.NonExistentFileException;
+import com.sammaru5.sammaru.repository.StorageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class FileRemoveService {
 
+    private final StorageRepository storageRepository;
+
     @Value("${app.fileDir}")
     private String fileDir;
 
-    boolean removeFile(Long boardId, String filePath){
+    //해당 파일 삭제
+    boolean removeFile(Long boardId, String filePath) throws NonExistentFileException {
+
         File targetFile = new File(fileDir + boardId + "/" + filePath);
-        if(!targetFile.exists()){
+        Optional<StorageEntity> storageEntity = storageRepository.findByFilePath(filePath);
+        if(!targetFile.exists() || !storageEntity.isPresent()){
             throw new NonExistentFileException();
         }
+
+        storageRepository.delete(storageEntity.get());
         return targetFile.delete();
     }
+
+    //Article에 속한 모든 파일 삭제
+    boolean removeFileByArticle(ArticleEntity articleEntity) throws NonExistentFileException {
+
+        List<StorageEntity> storageEntities = storageRepository.findByArticle(articleEntity);
+        for(StorageEntity storageEntity : storageEntities){
+            removeFile(articleEntity.getBoard().getId(), storageEntity.getFilePath());
+        }
+        return true;
+    }
+
 }

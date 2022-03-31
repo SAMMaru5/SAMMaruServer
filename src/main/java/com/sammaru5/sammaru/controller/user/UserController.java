3cafb522912f68bc1038472eece114314a510ad5
genius-jo <joks7285@daum.net>
2022-04-01 01:14:15 +0900

    feat : Get the user information

--- src/main/java/com/sammaru5/sammaru/controller/user/UserController.java
+++ src/main/java/com/sammaru5/sammaru/controller/user/UserController.java
@@ -3,18 +3,30 @@ package com.sammaru5.sammaru.controller.user;
 import com.sammaru5.sammaru.apiresult.ApiResult;
 import com.sammaru5.sammaru.request.RoleRequest;
 import com.sammaru5.sammaru.service.user.UserRoleService;
+import com.sammaru5.sammaru.service.user.UserSearchService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserRoleService userRoleService;
+    private final UserSearchService userSearchService;
 
-    @PatchMapping("/{userId}/role")
+    @PatchMapping("/users/{userId}/role")
     public ApiResult<?> roleChange(@RequestBody RoleRequest roleRequest, @PathVariable Long userId){
         return ApiResult.OK(userRoleService.changeRole(userId, roleRequest.getRole()));
+    }
+
+    @GetMapping("/user/info")
+    public ApiResult<?> userInfo(Authentication authentication) {
+        try{
+            return ApiResult.OK(userSearchService.getUserFromToken(authentication));
+        } catch(Exception e) {
+            return ApiResult.ERROR(e, HttpStatus.UNAUTHORIZED);
+        }
     }
 }

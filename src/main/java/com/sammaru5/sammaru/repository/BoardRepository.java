238f0ead779d9be43d2cbbba936b517367813d21
genius-jo <joks7285@daum.net>
2022-04-05 13:02:41 +0900

    chore : Convert the return value to Optional

--- src/main/java/com/sammaru5/sammaru/repository/BoardRepository.java
+++ src/main/java/com/sammaru5/sammaru/repository/BoardRepository.java
@@ -4,8 +4,10 @@ import com.sammaru5.sammaru.domain.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
 
-    BoardEntity findByName(String boardName);
+    Optional<BoardEntity> findByName(String boardName);
 }

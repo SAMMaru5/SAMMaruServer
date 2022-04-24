4e9512cc7c8d20ab0d874f771c69691d88bbd76f
genius-jo <joks7285@daum.net>
2022-04-24 15:29:48 +0900

    feat : Download the file using the file path

--- src/main/java/com/sammaru5/sammaru/controller/article/ArticleController.java
+++ src/main/java/com/sammaru5/sammaru/controller/article/ArticleController.java
@@ -7,15 +7,19 @@ import com.sammaru5.sammaru.service.article.ArticleModifyService;
 import com.sammaru5.sammaru.service.article.ArticleRegisterService;
 import com.sammaru5.sammaru.service.article.ArticleRemoveService;
 import com.sammaru5.sammaru.service.article.ArticleSearchService;
+import com.sammaru5.sammaru.service.file.FileStatusService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.io.IOException;
 
 @RestController @RequiredArgsConstructor
 @Api(tags = {"게시글 API"})
@@ -25,6 +29,7 @@ public class ArticleController {
     private final ArticleSearchService articleSearchService;
     private final ArticleRemoveService articleRemoveService;
     private final ArticleModifyService articleModifyService;
+    private final FileStatusService fileStatusService;
 
     @PostMapping(value="/api/boards/{boardId}/articles", consumes = {MediaType.APPLICATION_JSON_VALUE,MediaType.MULTIPART_FORM_DATA_VALUE})
     @ApiOperation(value = "게시글 생성", notes = "게시판에 게시글 추가", response = ArticleDTO.class)
@@ -48,5 +53,12 @@ public class ArticleController {
     @ApiOperation(value = "게시글 수정", notes = "작성자가 게시글 수정", response = ArticleDTO.class)
     public ApiResult<?> articleModify(Authentication authentication, @PathVariable Long boardId, @PathVariable Long articleId, @RequestPart(value="article", required = false) @Valid ArticleRequest articleRequest, @RequestPart(value="file", required = false) MultipartFile[] multipartFiles) {
         return ApiResult.OK(articleModifyService.modifyArticle(authentication, boardId, articleId, articleRequest, multipartFiles));
+    }
+
+    //파일 다운로드
+    @GetMapping("/api/boards/{boardId}/articles/{articleId}/file/{fileName:.+}")
+    @ApiOperation(value = "파일 다운로드", notes = "게시판 id와 파일 경로를 통해 파일 다운로드")
+    public ResponseEntity<InputStreamResource> fileDownload(@PathVariable Long boardId, @PathVariable Long articleId, @PathVariable String fileName) throws IOException {
+        return fileStatusService.downloadFile(boardId, fileName);
     }
 }
--- src/main/java/com/sammaru5/sammaru/service/file/FileStatusService.java
+++ src/main/java/com/sammaru5/sammaru/service/file/FileStatusService.java
@@ -1,23 +1,46 @@
 package com.sammaru5.sammaru.service.file;
 
-import com.sammaru5.sammaru.domain.ArticleEntity;
 import com.sammaru5.sammaru.domain.FileEntity;
 import com.sammaru5.sammaru.repository.FileRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Optional;
 
 @Service @RequiredArgsConstructor
 public class FileStatusService {
-    private final FileRepository fileRepository;
 
-    public List<FileEntity> findFilesByArticle(ArticleEntity articleEntity) {
-        List<FileEntity> findFiles = fileRepository.findByArticle(articleEntity);
-        if(findFiles.isEmpty()) {
-            return null;
-        } else {
-            return findFiles;
+    private final FileRepository fileRepository;
+    @Value("${app.fileDir}")
+    private String fileDir;
+
+    public ResponseEntity<InputStreamResource> downloadFile(Long boardId, String filePath) throws IOException {
+
+        Optional<FileEntity> fileEntity = fileRepository.findByFilePath(filePath);
+
+        if(!fileEntity.isPresent()) {
+            throw new IOException("해당 파일은 존재하지 않습니다");
         }
+
+        String path = fileDir + boardId + "/" + filePath;
+        File file = new File(path);
+        HttpHeaders header = new HttpHeaders();
+
+        header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename="+fileEntity.get().getFileName());
+        header.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        header.add("Pragma", "no-cache");
+        header.add("Expires", "0");
+
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+
+        return ResponseEntity.ok() .headers(header) .contentLength(file.length()) .contentType(MediaType.parseMediaType("application/octet-stream")) .body(resource);
     }
 }

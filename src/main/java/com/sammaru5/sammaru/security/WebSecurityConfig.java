7be83f8dc6b3b7544a8150fa2be2bf9ddbaa2943
chiyongs <tlscldyd@gmail.com>
2022-04-29 14:52:13 +0900

    fix : swagger version up 2 to 3

--- src/main/java/com/sammaru5/sammaru/security/WebSecurityConfig.java
+++ src/main/java/com/sammaru5/sammaru/security/WebSecurityConfig.java
@@ -48,7 +48,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) {
         web.ignoring()
-                .antMatchers("/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/swagger/**");
+                .antMatchers("/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/swagger-ui/**","/swagger/**");
     }
 
     @Override

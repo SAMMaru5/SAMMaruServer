5e59a2c0cda1bd8a671d97a35fb7e583e806a56a
chiyongs <tlscldyd@gmail.com>
2022-04-29 21:44:55 +0900

    feat : remove a comment

--- src/main/java/com/sammaru5/sammaru/controller/comment/CommentController.java
+++ src/main/java/com/sammaru5/sammaru/controller/comment/CommentController.java
@@ -4,6 +4,7 @@ import com.sammaru5.sammaru.apiresult.ApiResult;
 import com.sammaru5.sammaru.dto.CommentDTO;
 import com.sammaru5.sammaru.request.CommentRequest;
 import com.sammaru5.sammaru.service.comment.CommentRegisterService;
+import com.sammaru5.sammaru.service.comment.CommentRemoveService;
 import com.sammaru5.sammaru.service.comment.CommentSearchService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -20,6 +21,7 @@ public class CommentController {
 
     private final CommentSearchService commentSearchService;
     private final CommentRegisterService commentRegisterService;
+    private final CommentRemoveService commentRemoveService;
 
     @GetMapping("/api/boards/{boardId}/articles/{articleId}/comments")
     @ApiOperation(value = "댓글 목록 조회", notes = "해당 게시글에 달린 댓글 목록 불러오기", response = CommentDTO.class)
@@ -31,5 +33,11 @@ public class CommentController {
     @ApiOperation(value = "댓글 작성", notes = "해당 게시글에 댓글 작성", response = CommentDTO.class)
     public ApiResult<?> commentAdd(@PathVariable Long boardId, @PathVariable Long articleId, Authentication authentication, @Valid @RequestBody CommentRequest commentRequest) {
         return ApiResult.OK(commentRegisterService.addComment(authentication, commentRequest, articleId));
+    }
+
+    @DeleteMapping("/api/boards/{boardId}/articles/{articleId}/comments/{commentId}")
+    @ApiOperation(value = "댓글 삭제", notes = "해당 댓글 삭제", response = boolean.class)
+    public ApiResult<?> commentRemove(@PathVariable Long boardId, @PathVariable Long articleId, Authentication authentication, @PathVariable Long commentId) {
+        return ApiResult.OK(commentRemoveService.removeComment(authentication, commentId));
     }
 }
---
+++ src/main/java/com/sammaru5/sammaru/service/comment/CommentRemoveService.java
@@ -0,0 +1,31 @@
+package com.sammaru5.sammaru.service.comment;
+
+import com.sammaru5.sammaru.domain.CommentEntity;
+import com.sammaru5.sammaru.domain.UserEntity;
+import com.sammaru5.sammaru.repository.CommentRepository;
+import com.sammaru5.sammaru.service.user.UserStatusService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class CommentRemoveService {
+
+    private final CommentRepository commentRepository;
+    private final UserStatusService userStatusService;
+
+    public boolean removeComment(Authentication authentication, Long commentId) throws AccessDeniedException {
+        UserEntity user = userStatusService.getUser(authentication);
+        Optional<CommentEntity> comment = commentRepository.findById(commentId);
+        if (comment.get().getUser().getId().equals(user.getId())) {
+            commentRepository.deleteById(commentId);
+            return true;
+        } else {
+            throw new AccessDeniedException("해당 게시물에 권한이 없는 사용자 입니다");
+        }
+    }
+}

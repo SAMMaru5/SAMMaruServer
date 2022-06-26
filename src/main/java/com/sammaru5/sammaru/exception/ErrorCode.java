9e0a3fdf00353effe17398d7f34904b9d48a8654
Chisan Ahn <chisan2001@gmail.com>
2022-06-26 21:29:29 +0900

    refactor: file 관련 service들 CustomException 사용하도록 수정 (#18)

--- src/main/java/com/sammaru5/sammaru/exception/ErrorCode.java
+++ src/main/java/com/sammaru5/sammaru/exception/ErrorCode.java
@@ -16,6 +16,8 @@ public enum ErrorCode {
     ARTICLE_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 articleId 게시글이 존재하지 않습니다!"),
     BOARD_EMPTY(HttpStatus.BAD_REQUEST, "해당 게시판에 게시글이 존재하지 않습니다!"),
     COMMENT_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 commentId 댓글이 존재하지 않습니다!"),
+    FILE_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 파일은 존재하지 않습니다!");
+
 
     private final HttpStatus httpStatus;
     private final String detail;

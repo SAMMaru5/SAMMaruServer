4486a92244f44124c7f0d51a2921665426209664
genius-jo <joks7285@daum.net>
2022-07-01 01:31:57 +0900

    feat : 메서드 권한 체크를 위한 커스텀 어노테이션 제작

---
+++ src/main/java/com/sammaru5/sammaru/util/OverAdminRole.java
@@ -0,0 +1,14 @@
+package com.sammaru5.sammaru.util;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@PreAuthorize("hasRole('ROLE_ADMIN')")
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface OverAdminRole {
+}
---
+++ src/main/java/com/sammaru5/sammaru/util/OverTempRole.java
@@ -0,0 +1,14 @@
+package com.sammaru5.sammaru.util;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_MEMBER') or hasRole('ROLE_TEMP')")
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface OverTempRole {
+}

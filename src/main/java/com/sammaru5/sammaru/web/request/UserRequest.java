24d622a0323fd21dc3b6f37b63742e8eaf854eee
chisan01 <chisan2001@gmail.com>
2022-07-25 19:09:22 +0900

    fix: 회원 정보 수정 기능 client에 맞게 수정 (#58)

--- src/main/java/com/sammaru5/sammaru/web/request/UserRequest.java
+++ src/main/java/com/sammaru5/sammaru/web/request/UserRequest.java
@@ -3,13 +3,20 @@ package com.sammaru5.sammaru.web.request;
 import lombok.Getter;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 @Getter
 public class UserRequest {
+
+    @NotBlank(message = "이름은 필수 입력입니다")
     private String username;
+
+    @NotBlank(message = "비밀번호는 필수 입력입니다")
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}", message = "비밀번호는 8~20자로 영문, 숫자, 특수문자를 적어도 하나 사용해야 됩니다")
     private String password;
+
+    @NotBlank(message = "이메일은 필수 입력입니다")
     @Email(message = "이메일은 이메일 형식에 맞아야 됩니다")
     private String email;
 }

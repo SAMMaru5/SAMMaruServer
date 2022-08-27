46c45654b13a2d064f5ff4900075b80ab67989ee
chiyongs <tlscldyd@gmail.com>
2022-08-27 16:27:49 +0900

    chore : 사용하지 않는 import 제거 및 typo 제거

--- src/main/java/com/sammaru5/sammaru/config/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/com/sammaru5/sammaru/config/jwt/JwtAccessDeniedHandler.java
@@ -1,6 +1,6 @@
 package com.sammaru5.sammaru.config.jwt;
 
-import com.infomansion.server.global.util.exception.ErrorCode;
+import com.sammaru5.sammaru.exception.ErrorCode;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -15,14 +15,14 @@ public class JwtAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
         // 필요한 권한이 없이 접근 시 403
-        setResponse(response, ErrorCode.USER_NO_PERMISSION);
+        setResponse(response, ErrorCode.UNAUTHORIZED_USER_ACCESS);
     }
 
     private void setResponse(HttpServletResponse response, ErrorCode errorCode) throws IOException {
         response.setContentType("application/json;charset=UTF-8");
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-        response.getWriter().println("{ \"message\" : \"" + errorCode.getMessage()
-                + "\", \"code\" : \"" +  errorCode.getCode()
+        response.getWriter().println("{ \"message\" : \"" + errorCode.getDetail()
+//                + "\", \"code\" : \"" +  errorCode.getCode()
                 + "\", \"success\" : " + false
                 + "}");
     }

76c4fcb40fd7ada939c8b19e3280b21316747284
Jinyoung <74577889+jinyoung-2@users.noreply.github.com>
2022-09-04 10:32:28 +0900

    fix: username으로 검색시, 검색글자가 포함된 모든 users를 List로 출력되도록 수정 (#85)

--- src/main/java/com/sammaru5/sammaru/repository/UserRepository.java
+++ src/main/java/com/sammaru5/sammaru/repository/UserRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
+    List<User> findByUsernameContaining(String username);
 
     Optional<User> findByStudentId(String studentId);
     boolean existsByStudentId(String studentId);

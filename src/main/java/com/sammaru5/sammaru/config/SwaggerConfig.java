aae71e10a455d75af863e2f593951c191526ce0f
chisan01 <chisan2001@gmail.com>
2022-09-22 11:25:23 +0900

    docs: Swagger에서 테스트시 authorization 헤더 설정할 수 있는 기능 추가

--- src/main/java/com/sammaru5/sammaru/config/SwaggerConfig.java
+++ src/main/java/com/sammaru5/sammaru/config/SwaggerConfig.java
@@ -7,9 +7,16 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
 
 @EnableSwagger2
 @Configuration
@@ -28,10 +35,33 @@ public class SwaggerConfig {
     public Docket restAPI() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(List.of(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.sammaru5.sammaru.web.controller"))
                 .paths(PathSelectors.any()) //어떤 식으로 시작하는 api를 보여줄것인가
                 .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return springfox
+                .documentation
+                .spi.service
+                .contexts
+                .SecurityContext
+                .builder()
+                .securityReferences(defaultAuth()).forPaths(PathSelectors.any()).build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("JWT", authorizationScopes));
     }
 
     public ApiInfo apiInfo() {

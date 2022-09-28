af9e5bf383462602f23b3b6b4f669d2716128a23
yeongsang2 <dudtkd1221@gmail.com>
2022-09-28 22:02:14 +0900

    fix: CommentDTO수정 (createDt, author추가) (#113)

--- src/main/java/com/sammaru5/sammaru/web/dto/CommentDTO.java
+++ src/main/java/com/sammaru5/sammaru/web/dto/CommentDTO.java
@@ -4,15 +4,19 @@ import com.sammaru5.sammaru.domain.Comment;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import java.sql.Timestamp;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CommentDTO {
     private Long id;
     private String content;
-
+    private Timestamp createDt;
+    private String author;
     public CommentDTO(Comment comment) {
         this.id = comment.getId();
         this.content = comment.getContent();
+        this.createDt = comment.getCreateTime();
+        this.author =comment.getUser().getUsername();
     }
 }

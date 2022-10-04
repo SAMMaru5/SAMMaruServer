ae9bed50e58ca6e7cb6acb42470754f4eda430de
Jinyoung <74577889+jinyoung-2@users.noreply.github.com>
2022-10-04 20:55:01 +0900

    refactor: 게시판 조회 서비스 이름 변경 (#130)

--- src/main/java/com/sammaru5/sammaru/service/board/BoardSearchService.java
+++ src/main/java/com/sammaru5/sammaru/service/board/BoardSearchService.java
@@ -1,8 +1,6 @@
 package com.sammaru5.sammaru.service.board;
 
 import com.sammaru5.sammaru.domain.Board;
-import com.sammaru5.sammaru.exception.CustomException;
-import com.sammaru5.sammaru.exception.ErrorCode;
 import com.sammaru5.sammaru.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,7 +10,7 @@ import java.util.List;
 
 @Transactional(readOnly = true)
 @Service @RequiredArgsConstructor
-public class BoardStatusService {
+public class BoardSearchService {
     private final BoardRepository boardRepository;
 
     public List<Board> findBoards() {

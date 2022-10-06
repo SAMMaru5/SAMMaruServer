200e539c41a9070fbcda4da669d4e12b5b867ae3
Jinyoung <74577889+jinyoung-2@users.noreply.github.com>
2022-10-06 23:02:58 +0900

    fix: 댓글 및 일정 요청 파라미터 체크 위해 사용되는 어노테이션 추가 (#138)
    * fix: 댓글 요청 파라미터 체크 위해 사용되는 어노테이션 추가
    
    * fix: 일정 요청 파라미터 체크 위해 사용되는 어노테이션 추가

--- src/main/java/com/sammaru5/sammaru/web/request/CommentRequest.java
+++ src/main/java/com/sammaru5/sammaru/web/request/CommentRequest.java
@@ -2,7 +2,10 @@ package com.sammaru5.sammaru.web.request;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 public class CommentRequest {
+    @NotBlank(message = "내용은 필수 입력입니다")
     private String content;
 }

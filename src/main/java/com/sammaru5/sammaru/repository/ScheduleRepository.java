f28f669d681780c2c1a5b95264c09b00e9ef1de9
Jinyoung <74577889+jinyoung-2@users.noreply.github.com>
2022-10-06 13:57:56 +0900

    refactor: Repository 어노테이션 생략 (#140)

--- src/main/java/com/sammaru5/sammaru/repository/ScheduleRepository.java
+++ src/main/java/com/sammaru5/sammaru/repository/ScheduleRepository.java
@@ -2,12 +2,10 @@ package com.sammaru5.sammaru.repository;
 
 import com.sammaru5.sammaru.domain.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.sql.Date;
 import java.util.List;
 
-@Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
     List<Schedule> findByEndBetweenOrderByStartAscEndAsc(Date start, Date end);

c181a33397afa5e2768d0dc06cbe995e275eed66
JinYoungLee <brenda524@naver.com>
2022-10-06 22:14:11 +0900

    refactor: 게시판 등록 서비스내의 트랜잭션 단위안에서 예외처리하도록 설정

--- src/main/java/com/sammaru5/sammaru/service/board/BoardRegisterService.java
+++ src/main/java/com/sammaru5/sammaru/service/board/BoardRegisterService.java
@@ -1,6 +1,5 @@
 package com.sammaru5.sammaru.service.board;
 
-import com.sammaru5.sammaru.domain.Board;
 import com.sammaru5.sammaru.exception.CustomException;
 import com.sammaru5.sammaru.exception.ErrorCode;
 import com.sammaru5.sammaru.web.dto.BoardDTO;
@@ -10,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 
 @Transactional
 @Service @RequiredArgsConstructor
@@ -18,11 +16,12 @@ public class BoardRegisterService {
     private final BoardRepository boardRepository;
 
     public BoardDTO addBoard(BoardRequest boardRequest) {
-        validateDuplicateBoard(boardRequest.getBoardName());
-        return BoardDTO.from(boardRepository.save(boardRequest.toEntity()));
-    }
+        String boardName = boardRequest.getBoardName();
 
-    private void validateDuplicateBoard(String boardName) {
-        if(boardRepository.existsByBoardName(boardName)) throw new CustomException(ErrorCode.ALREADY_EXIST_BOARD);
+        if(boardRepository.existsByBoardName(boardRequest.getBoardName())) {
+            throw new CustomException(ErrorCode.ALREADY_EXIST_BOARD, boardRequest.getBoardName());
+        }
+
+        return BoardDTO.from(boardRepository.save(boardRequest.toEntity()));
     }
 }

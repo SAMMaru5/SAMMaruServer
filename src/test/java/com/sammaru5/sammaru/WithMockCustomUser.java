a7c177694ad549438d44f78cd395527572a1b100
chisan01 <chisan2001@gmail.com>
2022-10-12 16:49:26 +0900

    refactor: WithMockCustomUser Annotation에 DirtiesContext Annotation이 포함되도록 수정
    - 현재는 WithMockCustomUser를 사용해서 테스트를 할때만 DirtiesContext Annotation을 사용하기 때문에 해당 어노테이션에 포함시킴으로써 코드 중복 제거

--- src/test/java/com/sammaru5/sammaru/WithMockCustomUser.java
+++ src/test/java/com/sammaru5/sammaru/WithMockCustomUser.java
@@ -2,12 +2,14 @@ package com.sammaru5.sammaru;
 
 import com.sammaru5.sammaru.domain.UserAuthority;
 import org.springframework.security.test.context.support.WithSecurityContext;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
 @WithSecurityContext(factory = WithMockCustomUserSecurityContextFactory.class)
+@DirtiesContext(methodMode = DirtiesContext.MethodMode.BEFORE_METHOD)
 public @interface WithMockCustomUser {
 
     String userId() default "1";
